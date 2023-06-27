df23b02693a92c20bac8f4c33ee33c331b7e4b56
Kunal Kankariya <kkankariya007@gmail.com>
2023-06-27 17:09:21 +0530

    Commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,33 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:richh/services/people.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
-
-
-
+  List<PeopleList> ppl=[
+    PeopleList(img:'billgates.jpg', name:'Bill Gates', money:50000),
+    PeopleList(img:'elon', name:'Elon Musk', money:503403),
+    PeopleList(img:'mukesh.png',  name:'Mukesh Ambani', money:5343),
+    PeopleList(img: 'kunal.png', name:'K', money:3214),
+    PeopleList(img: 'jk.png', name: 'jssl', money: 324),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text('Richiee'),
+      title: Text('Re'),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.black.withOpacity(0.8),
       ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0.0,8.0,0.0,0.0),
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisExtent: 300,// Number of columns in the grid
+            ),
+            itemCount: ppl.length,
+            itemBuilder: (BuildContext context, int index) {
+              var person = ppl[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:Column(
+                  children: <Widget>[
+                    InkWell(
+                    onTap:() {
+                      // Navigator.pushReplacementNamed(context, routeName)
+              print("hello");
+              },
+                child: Image.asset('images/${ppl[index].img}',
+                ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text(ppl[index].name,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),),
+                    ),
+                    Text('\$${ppl[index].money}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),),
+                  ],
+                ),
+
+              );
+}
+                       ),
+                    // Text(ppl[index].name),
 
 
-      body:Center(
-        child: Text('This is the first way'),
-      ),
+
+              ),
+    ),
       backgroundColor: Colors.black,
     );
+
   }
 }
---
+++ lib/services/people.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PeopleList
+{
+  String img;
+  String name;
+  int money;
+
+  PeopleList({required this.img,required this.name,required this.money});
+}
